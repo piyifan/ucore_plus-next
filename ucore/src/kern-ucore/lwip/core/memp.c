d4137ce2e2bb0b6567b1e9f9f1e217e191ca4d3e
hs3180 <thuhuangs09@gmail.com>
2012-11-20 04:47:00 +0800

    split trace print

--- ucore/src/kern-ucore/lwip/core/memp.c
+++ ucore/src/kern-ucore/lwip/core/memp.c
@@ -285,12 +285,13 @@ memp_init(void)
         + MEMP_SANITY_REGION_AFTER_ALIGNED
 #endif
       );
-  LWIP_DEBUGF(MEMP_DEBUG , ("memp_init: alloc a memp elements\n"));
-  LWIP_DEBUGF(MEMP_DEBUG , ("memp_init: SIZE = %d\n", MEMP_SIZE + memp_sizes[i]
+  LWIP_DEBUGF(MEMP_DEBUG , ("memp_init: alloc a memp element\n"));
+  LWIP_DEBUGF(MEMP_DEBUG , ("memp_init: SIZE = %d\n", MEMP_SIZE + memp_sizes[i]));
+/*
 #if MEMP_OVERFLOW_CHECK
         + MEMP_SANITY_REGION_AFTER_ALIGNED
 #endif
-));
+));*/
     }
   }
 #if MEMP_OVERFLOW_CHECK
